7fde7a27ab3673bfb200325151c3590b4ac455c2
Donkeyboy223 <sdoncaster5@gmail.com>
2024-03-02 13:34:52 +0000

    fixes

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv').config()
 const cors = require('cors');
 const mysql = require('mysql2');
 const app = express();
-const port = "https://urlshortner-backend-nine.vercel.app/";
+const port = "https://long-blue-angelfish-coat.cyclic.app/";
 
 const databaseUser = process.env.DATABASE_USER;
 const databasePass = process.env.DATABASE_PASS;
